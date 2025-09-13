26b214279be575b44322869169e79f919db3d390
Alyasaf Meireles <gasafm62@gmail.com>
2025-09-13 17:30:27 -0300

    feat: implements class and inner class for configurations Abacatepay

--- src/main/java/com/github/Alym62/sdk/configurations/AbacatePayConfiguration.java
+++ src/main/java/com/github/Alym62/sdk/configurations/AbacatePayConfiguration.java
@@ -1,10 +1,85 @@
 package com.github.Alym62.sdk.configurations;
 
-import lombok.AllArgsConstructor;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.retry.RetryConfig;
 import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
+import java.time.Duration;
+
 public class AbacatePayConfiguration {
-    private String apiKey;
+    @Getter
+    private final String apiKey;
+
+    @Getter
+    private final RetryConfig retryConfig;
+
+    @Getter
+    private final CircuitBreakerConfig circuitBreakerConfig;
+
+    private AbacatePayConfiguration(DefaultConfigurationCircuitAndRetry defaultConfiguration) {
+        this.retryConfig = defaultConfiguration.retryConfig;
+        this.circuitBreakerConfig = defaultConfiguration.circuitBreakerConfig;
+        this.apiKey = defaultConfiguration.apiKey;
+    }
+
+    private AbacatePayConfiguration(CustomConfigurationCircuitAndRetry customConfiguration) {
+        this.retryConfig = customConfiguration.retryConfig;
+        this.circuitBreakerConfig = customConfiguration.circuitBreakerConfig;
+        this.apiKey = customConfiguration.apiKey;
+    }
+
+    public static final class DefaultConfigurationCircuitAndRetry extends ApiKeyConfiguration {
+        private final RetryConfig retryConfig = RetryConfig.custom()
+                .maxAttempts(3)
+                .waitDuration(Duration.ofSeconds(2))
+                .build();
+
+        private final CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofSeconds(10))
+                .slidingWindowSize(5)
+                .build();
+
+        public DefaultConfigurationCircuitAndRetry apiKey(final String apiKey) {
+            this.apiKey = apiKey;
+            return this;
+        }
+
+        public AbacatePayConfiguration build() {
+            return new AbacatePayConfiguration(this);
+        }
+    }
+
+    public static final class CustomConfigurationCircuitAndRetry extends ApiKeyConfiguration {
+        private CircuitBreakerConfig circuitBreakerConfig;
+        private RetryConfig retryConfig;
+
+        public CustomConfigurationCircuitAndRetry circuitBreakerConfig(final CircuitBreakerConfig circuitBreakerConfig) {
+            this.circuitBreakerConfig = circuitBreakerConfig;
+            return this;
+        }
+
+        public CustomConfigurationCircuitAndRetry retryConfig(final RetryConfig retryConfig) {
+            this.retryConfig = retryConfig;
+            return this;
+        }
+
+        public CustomConfigurationCircuitAndRetry apiKey(final String apiKey) {
+            this.apiKey = apiKey;
+            return this;
+        }
+
+        public AbacatePayConfiguration build() {
+            return new AbacatePayConfiguration(this);
+        }
+    }
+
+    abstract static class ApiKeyConfiguration {
+        protected String apiKey;
+
+        public ApiKeyConfiguration apiKey(final String apiKey) {
+            this.apiKey = apiKey;
+            return this;
+        }
+    }
 }
